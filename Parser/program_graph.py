77891f416668160959098ac1551b366df95eac8b
Robin TROESCH <38283096+unitrium@users.noreply.github.com>
2021-10-04 14:44:39 +0200

    pep8 + description of datastructure

--- Parser/program_graph.py
+++ Parser/program_graph.py
@@ -1,6 +1,6 @@
 """Converts AST to program graph."""
-
+"""Program graph structure is a dict whose keys are the index of the nodes. Under each item is a dict with nodes reachable from this node and action."""
 
 program_graph = {
-    
+
 }
