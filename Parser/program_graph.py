b3cc07cdbbeeb250ba9ebb808b55b4536a669324
Robin TROESCH <38283096+unitrium@users.noreply.github.com>
2021-10-20 00:14:27 +0200

    add check edges

--- Parser/program_graph.py
+++ Parser/program_graph.py
@@ -76,6 +76,22 @@ def high_level_edges(tree: Tree):
         raise Exception("Supplied tree does not start with program.")
 
 
+def check_edges(edges: List[Edge]):
+    """Check the coherence of a graph."""
+    flat = {}
+    for edge in edges:
+        if edge.start.number not in flat:
+            flat[edge.start.number] = id(edge.start)
+        elif flat[edge.start.number] != id(edge.start):
+            raise Exception(
+                f"Incoherent graph. Node {edge.start.number} is present twice but with different objects.")
+        if edge.end.number not in flat:
+            flat[edge.end.number] = id(edge.end)
+        elif flat[edge.end.number] != id(edge.end):
+            raise Exception(
+                f"Incoherent graph. Node {edge.end.number} is present twice but with different objects.")
+
+
 def expand_a_expr(tree: Tree) -> List[str]:
     """Given an a_expr tree unfolds the a_expr"""
     expr = []
--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from Parser.parser import grammar
-from Parser.program_graph import high_level_edges, Node
+from Parser.program_graph import high_level_edges, check_edges
 
 program = "if (a < 2) {write a;} read a; if (b==2) {write x;}"
 tree = grammar.parse(program)
 print(tree.children)
 
 edges = high_level_edges(tree)
+check_edges(edges)
 for edge in edges:
     print(edge)
