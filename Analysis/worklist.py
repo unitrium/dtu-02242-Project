2e5a2203eea136bfa268474d59856d5e05f5b53a
Robin TROESCH <38283096+unitrium@users.noreply.github.com>
2021-11-08 16:02:37 +0100

    create reverse postorder

--- Analysis/worklist.py
+++ Analysis/worklist.py
@@ -1,1 +1,24 @@
-from Parser.program_graph import Node, Edge
+from typing import Dict, List, Set
+from Parser import ProgramGraph, Node
+from Parser.program_graph import Edge
+
+
+def compute_reverse_post_order(prgmGraph: ProgramGraph):
+    nodes = prgmGraph.get_nodes()
+    edges = prgmGraph.get_edges()
+    rP = {}
+    t = set()
+    visited = set()
+    k = len(nodes)
+    dfs(nodes[0], visited, edges, k, rP, t)
+    return t, rP
+
+
+def dfs(node: Node, visited: Set[Node], edges: List[Edge], k: int, rP: Dict, t: set):
+    visited.add(node.number)
+    for edge in node.outgoing_edges:
+        if edge.end.number not in visited:
+            t.add(f"{edge.start.number}, {edge.end.number}")
+            rP[edge.start.number] = k
+            k = k-1
+            dfs(node=edge.end, visited=visited, edges=edges, k=k, rP=rP, t=t)
