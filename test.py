2e3e6f9f6e071a3b0fe19a1b255f0ff6c0b3d9c5
shishir <ccrsharma7@gmail.com>
2021-11-16 21:51:40 +0100

    small change in test file

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ from Parser import ProgramGraph
 from Analysis import solve_chaotic_iteration, ReachingDefintionAnalysis, LiveVariableAnalysis, worklist
 
 # if even write 0 else write 1 and when odd goes to loop and writes 007.
-program = "int a; a := 11; b := false; if ( a/2 == 0) { write 0; } else { b:= true; write 1; } while(b) { write 007; }"
+program = "int a; a := 11; b := false; if ( a/2 == 0) { write 0; } else { b:= true; write 1; } while(b) { b:=false; write 007; }"
 
 
 pg = ProgramGraph(program)
