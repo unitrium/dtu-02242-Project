b4a0328f5ddc58c6e1af1c8a8c1b0d1afe34c91d
Robin TROESCH <38283096+unitrium@users.noreply.github.com>
2021-11-30 23:56:08 +0100

    use txt

--- main.py
+++ main.py
@@ -4,11 +4,14 @@ from Analysis.utils import display_assignment
 from Parser import ProgramGraph
 from Analysis import ReachingDefintionAnalysis, LiveVariableAnalysis, worklist, SignDetectionAnalysis
 
-program = "int[3] A; int a; int b; int c; int x; {int fst; int snd} R; if (a < 2) {write a;} read a; if (b==2) {write x;} write R.fst; A[a+1]:= 0 + a - c;"
+#program = "int[3] A; int a; int b; int c; int x; {int fst; int snd} R; if (a < 2) {write a;} read a; if (b==2) {write x;} write R.fst; A[a+1]:= 0 + a - c;"
+with open("toyProgram.txt", "r") as text:
+    pg = ProgramGraph(text.read())
+    for edge in pg.edges:
+        print(edge)
 
-pg = ProgramGraph(program)
-assignment, steps = worklist(
-    pg, SignDetectionAnalysis, RoundRobinAlgorithm)
+    assignment, steps = worklist(
+        pg, SignDetectionAnalysis, RoundRobinAlgorithm)
 
-print(steps)
-display_assignment(assignment)
+    print(steps)
+# display_assignment(assignment)
